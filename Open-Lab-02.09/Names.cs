c4b3fc7b9f3c03978cf440fb7a7b9f6e5c716955
samuelmalik <samuelmalik9@gmail.com>
2022-09-20 10:04:00 +0200

    adeed function returning single string

--- Open-Lab-02.09/Names.cs
+++ Open-Lab-02.09/Names.cs
@@ -6,7 +6,7 @@ namespace Open_Lab_02._09
     {
         public string ConcatName(string firstName, string lastName)
         {
-            throw new NotImplementedException();
+            return lastName + ", " + firstName;
         }
     }
 }
